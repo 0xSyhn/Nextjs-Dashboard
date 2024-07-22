76f52f3bdb1cde8e2c314756298b20e790f78c8c
Sayhan Ali <97283226+nocluee@users.noreply.github.com>
2024-07-22 14:40:32 +0530

    update invoice function added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({id:true, date:true});
+const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
 export async function createInvoices(formData: FormData){
     const {customerId,amount,status} = CreateInvoice.parse({
@@ -28,6 +29,23 @@ export async function createInvoices(formData: FormData){
     await sql`
     INSERT INTO invoices (customer_id,amount,status, date)
     VALUES(${customerId}, ${amount}, ${status}, ${date})
+    `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id:string, formData:FormData){
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId:formData.get('customerId'),
+        amount:formData.get('amount'),
+        status:formData.get('status'),
+    })
+    const amountInCents = amount*100;
+    
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId},amount = ${amount},status=${status}
+    WHERE id =${id}
     `;
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
